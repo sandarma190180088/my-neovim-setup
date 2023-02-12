d3654f392372e440767ab75790d90177862fe05d
sandarma190180088 <sandarma190180088@mhs.unimal.ac.id>
2023-02-12 23:10:57 +0700

    fixed telescope

--- after/plugin/alpha.lua
+++ after/plugin/alpha.lua
@@ -15,9 +15,9 @@ dashboard.section.header.val = {
 dashboard.section.buttons.val = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("t", "  Telescope", ":Telescop<CR>"),
 	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("T", "  Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
