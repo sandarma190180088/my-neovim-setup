deb66b49aaa436395dbcc4a8f9f06c78d488a4fd
sandarma190180088 <sandarma190180088@mhs.unimal.ac.id>
2023-03-05 08:37:42 +0700

    add filetype in coderunner and update lazy package

--- after/plugin/coderunner.lua
+++ after/plugin/coderunner.lua
@@ -1,9 +1,11 @@
 require('code_runner').setup({
   -- put here the commands by filetype
+  focus = false,
   filetype = {
 		java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
 		python = "python3 -u",
 		typescript = "deno run",
-		rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt"
+		rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt",
+		go="go run"
 	},
 })
